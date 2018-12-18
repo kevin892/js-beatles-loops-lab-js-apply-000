61706aec46e02daa649bf573bc36c2cd74d2455b
Kevin Ruggiero <kevinbruggiero@gmail.com>
2018-12-18 23:53:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let emArray = [];
+  for (let i = 0; i < musicians.length; i++) {
+    emArray.push(musicians[i] + "plays" + instruments[i]);
+  };
+
+return emArray;  
+};
