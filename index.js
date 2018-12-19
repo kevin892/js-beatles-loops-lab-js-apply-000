1376b7fa1a56d86bfc58d5a7870c448381276b05
Kevin Ruggiero <kevinbruggiero@gmail.com>
2018-12-19 00:56:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ function theBeatlesPlay(musicians, instruments) {
 return emArray;
 };
 
-function johnLennonFacts(fact) {
+function johnLennonFacts(facts) {
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
   let lennonArray = [];
   let j = 0;
-  while (j < fact.length) {
-    lennonArray.push(`${fact[j]}!!!`);
+  while (j < facts.length) {
+    lennonArray.push(`${facts[j]}!!!`);
     j++;
   };
   return lennonArray;
